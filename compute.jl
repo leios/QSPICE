cf74b086c0db5349ad7cf41061cb93b65f2adff8
leios <jrs.schloss@gmail.com>
2016-02-15 07:00:31 +0900

    Implemented rotation gate
    1. Rotation gate will act as phase flip with theta = pi
    2. fixed index order for bloch sphere

--- compute.jl
+++ compute.jl
@@ -8,8 +8,8 @@
 # First, the qubit type. Defined on Bloch sphere
 type bloch
     r::Float64
-    theta::Float64
     phi::Float64
+    theta::Float64
 end
 
 # Now to hold the information from the qubit
@@ -49,20 +49,32 @@ function hadamard(bit::qubit)
     return bit
 end
 
+# Implementing the not gate
+function not(bit::qubit)
+    N_gate = [Complex(0) Complex(1); Complex(1) Complex(0)]
+    bit.element = N_gate * bit.element
+    return bit
+end
+
+# Implementing a rotation gate
+function rotation(bit::qubit, theta)
+    R_gate = [Complex(1) Complex(0); Complex(0) Complex(exp(im * theta))]
+    bit.element = R_gate * bit.element
+    return bit
+end
+
 #=-----------------------------------------------------------------------------#
 # MAIN
 #-----------------------------------------------------------------------------=#
 
-bit_bloch = bloch(1,0,0)
+bit_bloch = bloch(1,0,pi)
 bit = bloch_to_qubit(bit_bloch)
-bit = hadamard(bit)
+println(bit.element[1], '\t', bit.element[2])
+bit = rotation(bit, pi)
 println(bit.element[1], '\t', bit.element[2])
 
 # List of functions that need to be implemented
 #=
-function hadamard()
-end
-
 function pauli_x()
 end
 
@@ -76,9 +88,6 @@ function swap()
 end
 
 function xor()
-end
-
-function rotation()
 end
 
 function measure()
